610b96fb164950899b89bca180274cc0af2a3598
jun.xu <jun.xu@yuneec.com>
2018-01-23 07:28:56 +0800

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Justin Xu on 2018/1/23.
+//  Copyright © 2018年 Justin Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Justin Xu on 2018/1/23.
+//  Copyright © 2018年 Justin Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
